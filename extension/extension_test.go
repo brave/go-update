154aec94669be636b5798d37629a1988cfe5b2c3
Brian R. Bondy <netzen@gmail.com>
2018-08-02 13:47:25 -0400

    Add test for Contains

--- extension/extension_test.go
+++ extension/extension_test.go
@@ -5,6 +5,27 @@ import (
 	"testing"
 )
 
+func TestContains(t *testing.T) {
+	extension1 := OfferedExtensions[0]
+	extension2 := OfferedExtensions[1]
+	unknownExtension := extension1;
+	unknownExtension.ID = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+	allExtensions := []Extension{extension1, extension2}
+	extension, err := Contains(allExtensions, extension1.ID)
+	extensiontest.AssertEqual(t, err, nil)
+	extensiontest.AssertEqual(t, extension.ID, extension1.ID)
+
+	extension, err = Contains(allExtensions, extension2.ID)
+	extensiontest.AssertEqual(t, err, nil)
+	extensiontest.AssertEqual(t, extension.ID, extension2.ID)
+
+	extension, err = Contains(allExtensions, unknownExtension.ID)
+	if err == nil {
+	  t.Fatalf("Not found extension should throw an error")
+	}
+}
+
 func TestCompareVersions(t *testing.T) {
 	// 3 component versions match
 	extensiontest.AssertEqual(t, CompareVersions("1.1.1", "1.1.1"), 0)
